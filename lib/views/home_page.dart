f97baf40cc5589c3eefc13dd12451491b2302e6a
Chares Lemos <lemos.charles@hotmail.com>
2021-11-25 23:12:34 -0300

    primerio commit

---
+++ lib/views/home_page.dart
@@ -0,0 +1,409 @@
+// ignore_for_file: prefer_const_constructors, prefer_final_fields, use_key_in_widget_constructors, deprecated_member_use, sized_box_for_whitespace, avoid_print, unused_element, unnecessary_cast, unused_import, unused_local_variable, prefer_const_literals_to_create_immutables, duplicate_ignore, unrelated_type_equality_checks, unused_field, non_constant_identifier_names, await_only_futures, prefer_typing_uninitialized_variables
+
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:another_flushbar/flushbar.dart';
+import 'package:conversor_moedas_flutter/models/result_cripto.dart';
+
+import 'package:conversor_moedas_flutter/services/via_cripto_service.dart';
+import 'package:flutter/material.dart';
+import 'package:share/share.dart';
+
+import 'package:conversor_moedas_flutter/models/result_cep.dart';
+import 'package:conversor_moedas_flutter/services/via_cep_service.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // Declaração Bools
+  bool _loading = false;
+  bool _enableField = true;
+  bool _erro = true;
+
+  // Declaração Strings via CEP
+  String? _result;
+  String? _resultCripto;
+  String? status = 'Favor, informar o CEP';
+  String? cep = '';
+  String? logradouro = '';
+  String? complemento = '';
+  String? bairro = '';
+  String? localidade = '';
+  String? uf = '';
+  String? ibge = '';
+  String? gia = '';
+  String? ddd = '';
+  String? siafi = '';
+
+  // Declarações Strings via Criptos
+
+  String? Btc_buy;
+  String? Ltc_buy;
+  String? Ada_buy;
+  String? Uni_buy;
+  String? Usdc_buy;
+
+  // Declaração Finals
+  final formKey = GlobalKey<FormState>();
+  final List<Flushbar> flushBars = [];
+
+  // Var auxiliares
+  var titleSnackBar = '';
+  var massageSnackBar = '';
+  var comparar = '93900-000';
+  var _searchCepController = TextEditingController();
+
+  get child => null;
+
+  @override
+  void dispose() {
+    super.dispose();
+    _searchCepController.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('CONVERSOR DE CRIPTO'),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            _buildSearchCepTextField(),
+            _buildSearchCepButton(),
+            _buildResultForm()
+          ],
+        ),
+      ),
+    );
+  }
+
+  ///////////////////////////////////////////////////////////////////////
+
+  Widget _buildSearchCepTextField() {
+    return TextField(
+      autofocus: true,
+      keyboardType: TextInputType.number,
+      textInputAction: TextInputAction.done,
+      decoration: InputDecoration(labelText: 'Informe o Cep'),
+      controller: _searchCepController,
+      enabled: _enableField,
+    );
+  }
+
+  Widget _buildSearchCepButton() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 30.0),
+      child: RaisedButton(
+        onPressed: _searchCep,
+        child: _loading
+            ? _circularLoading()
+            : Text(
+                'Consultar',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+      ),
+    );
+  }
+
+  Widget _circularLoading() {
+    return Container(
+      height: 15.0,
+      width: 15.0,
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  Widget _buildResultForm() {
+    return Card(
+      elevation: 3,
+      margin: const EdgeInsets.all(15),
+      child: InkWell(
+        onTap: () {},
+        child: Padding(
+          padding: const EdgeInsets.all(25),
+          child: Row(
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              IconButton(
+                icon: const Icon(
+                  Icons.share,
+                  color: Colors.blueGrey,
+                ),
+                onPressed: () {
+                  _share(context);
+                },
+              ),
+
+              SizedBox(
+                width: 10,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  Text(
+                    '$status',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    '               ',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    '$logradouro',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                  Text(
+                    '$complemento',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                  Text(
+                    '$bairro',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                  Text(
+                    '$localidade',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                  Text(
+                    '$uf',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                  Text(
+                    '$ddd',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                  Text(
+                    '$ibge',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ],
+              ),
+              Spacer(),
+              //Icon(
+              //  Icons.arrow_forward_ios_outlined,
+              //  size: 15,
+              //  color: Colors.green,
+              //),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future _searchCep() async {
+    // Variaveis de apoio
+    var cep = _searchCepController.text;
+
+    /********************************************************/
+
+    var cripto = '';
+    var resultCripto;
+
+    // Chama cripto BTC
+    cripto = 'BTC';
+    resultCripto = await ViaCriptoService.fetchCripto(cripto);
+    Btc_buy = resultCripto.ticker.buy;
+    print('PREÇO BTC: $Btc_buy');
+
+    // Chama cripto LTC
+    cripto = 'LTC';
+    resultCripto = await ViaCriptoService.fetchCripto(cripto);
+    Ltc_buy = resultCripto.ticker.buy;
+    print('PREÇO LTC: $Ltc_buy');
+
+    // Chama cripto ADA
+    cripto = 'ADA';
+    resultCripto = await ViaCriptoService.fetchCripto(cripto);
+    Ada_buy = resultCripto.ticker.buy;
+    print('PREÇO ADA: $Ada_buy');
+
+    // Chama cripto UNI
+    cripto = 'UNI';
+    resultCripto = await ViaCriptoService.fetchCripto(cripto);
+    Uni_buy = resultCripto.ticker.buy;
+    print('PREÇO UNI: $Uni_buy');
+
+    // Chama cripto USDC
+    cripto = 'USDC';
+    resultCripto = await ViaCriptoService.fetchCripto(cripto);
+    Usdc_buy = resultCripto.ticker.buy;
+    print('PREÇO USDC: $Usdc_buy');
+
+    /********************************************************/
+
+    // Validações
+    if (cep.isEmpty || cep == '' || cep.length > 8 || cep.length < 8) {
+      if (cep.length > 8 || cep.length < 8) {
+        _erro = true;
+        titleSnackBar = 'Atenção!';
+        massageSnackBar = 'Quantidade de caracter incorreto.';
+        showTopSnackBar(context);
+      }
+
+      if (cep.isEmpty || cep == '') {
+        _erro = true;
+        titleSnackBar = 'Atenção!';
+        massageSnackBar = 'Campo em branco. Favor, informe o CEP para busca.';
+        showTopSnackBar(context);
+      }
+
+      status = '';
+      cep = '';
+      logradouro = '';
+      complemento = '';
+      bairro = '';
+      localidade = '';
+      uf = '';
+      ibge = '';
+      gia = '';
+      ddd = '';
+      siafi = '';
+      //
+    } else {
+      //
+      _searching(true);
+
+      final resultCep = await ViaCepService.fetchCep(cep: cep);
+      _result = resultCep.toJson() as String?;
+
+      setState(() {
+        // Valida retorno do JSON
+
+        if (resultCep.cep == comparar) {
+          _erro = true;
+          titleSnackBar = 'Atenção!';
+          massageSnackBar = 'CEP inexistente!';
+          showTopSnackBar(context);
+
+          status = '';
+          cep = '';
+          logradouro = '';
+          complemento = '';
+          bairro = '';
+          localidade = '';
+          uf = '';
+          ibge = '';
+          gia = '';
+          ddd = '';
+          siafi = '';
+        } else {
+          _erro = false;
+          titleSnackBar = 'LEGAL!';
+          massageSnackBar = 'Busca realizada com sucesso.';
+          showTopSnackBar(context);
+          status = 'Resultado da busca';
+          cep = resultCep.cep;
+          logradouro = resultCep.logradouro;
+          complemento = resultCep.complemento;
+          bairro = resultCep.bairro;
+          localidade = resultCep.localidade;
+          uf = resultCep.uf;
+          ibge = resultCep.ibge;
+          gia = resultCep.gia;
+          ddd = resultCep.ddd;
+          siafi = resultCep.siafi;
+        }
+      });
+    }
+
+    _searching(false);
+    //
+  }
+
+  void _searching(bool enable) {
+    setState(() {
+      _result = enable ? '' : _result;
+      _loading = enable;
+      _enableField = !enable;
+    });
+  }
+
+  void showTopSnackBar(BuildContext context) => show(
+        context,
+        Flushbar(
+          icon: Icon(
+            Icons.error,
+            size: 40,
+            color: Colors.red,
+          ),
+
+          shouldIconPulse: false,
+          title: titleSnackBar,
+          message: massageSnackBar,
+
+          onTap: (_) {
+            print('Clicked bar');
+          },
+          duration: Duration(seconds: 3),
+          flushbarPosition: FlushbarPosition.TOP,
+          margin: EdgeInsets.fromLTRB(8, kToolbarHeight + 8, 8, 0),
+          //borderRadius: 16,
+        ),
+      );
+
+  Future show(BuildContext context, Flushbar newFlushBar) async {
+    await Future.wait(flushBars.map((flushBar) => flushBar.dismiss()).toList());
+    flushBars.clear();
+
+    newFlushBar.show(context);
+    flushBars.add(newFlushBar);
+  }
+
+  void _share(BuildContext context) {
+    dynamic cep;
+
+    if (_erro == true) {
+      titleSnackBar = 'Atenção!';
+      massageSnackBar = 'Não é possível compartilhar um CEP inválido.';
+      showTopSnackBar(context);
+    } else if (_result != '') {
+      cep = ResultCep.fromJson(_result!);
+      Share.share(
+        "cep: ${cep.cep}, Logradouro: ${cep.logradouro}, Complemento: ${cep.complemento},"
+        "Bairro: ${cep.bairro}, Cidade: ${cep.localidade}, Uf: ${cep.uf}, Ibge: ${cep.ibge},"
+        "Gia: ${cep.gia}, DDD: ${cep.ddd}, Siafi: ${cep.siafi}.",
+      );
+    }
+  }
+}
